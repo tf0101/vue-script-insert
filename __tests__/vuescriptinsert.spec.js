dc3858577145f25d45ab49fa9d7ecef6b651f8e5
tf0101 <tf0101_sh@icloud.com>
2021-05-13 06:52:30 +0900

    #4 Unit test, remake:Attaching components to the DOM

--- __tests__/vuescriptinsert.spec.js
+++ __tests__/vuescriptinsert.spec.js
@@ -2,20 +2,25 @@ import { mount } from '@vue/test-utils';
 import VueScriptInsert from '@/components/VueScriptInsert.vue'
 
 describe('AdvBlock', () => {
-    it('is a Vue instance', () => {
-      const wrapper = mount(VueScriptInsert,{
-          propsData:{
-            url:'<script>document.write("adv block");</scr'+'ipt>',
-            id:'toppage-topbanner',
-            layout:{
-                display: "block",
-                margin: "0 auto", 
-                background: "white",
-                width: "1080px",
-                height: "110px",
-              },
-          },
-      })
-      expect(wrapper.vm).toBeTruthy()
+
+  it('is a AdvBlock html', () => {
+    const wrapper = mount(VueScriptInsert,{
+      propsData:{
+        url:'<script>document.write("adv block");<\/script>',
+        id:'toppage-topbanner',
+        layout:{
+          display: "block",
+          margin: "0 auto", 
+          background: "white",
+          width: "1080px",
+          height: "110px",
+        },
+      },
+      //Attaching components to the DOM
+      attachTo: document.body
     })
+
+    expect(wrapper.html()).toBe('<div id="toppage-topbanner" class="adv-block-layout"><iframe></iframe></div>')
   })
+  
+})
