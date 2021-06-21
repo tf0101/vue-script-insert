fd691354295292d5accf222b887d5eaad3c4b55c
tf0101 <tf0101_sh@icloud.com>
2021-06-21 23:44:57 +0900

    #7 Create an overlay ad component.

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,62 @@
 <template>
   <div id="app">
     <div>
-       <VueScriptInsert :url="adv_url_top" :id="adv_id_top" :layout="adv_layout"></VueScriptInsert>
+      <OverlayAdvTop :overlayid="adv_id_overlay_top" :url="adv_url_top" :id="adv_id_top" :layout="adv_layout"></OverlayAdvTop>
+      <OverlayAdvBottom :overlayid="adv_id_overlay_bottom" :url="adv_url_top" :id="adv_id_bottom" :layout="adv_layout"></OverlayAdvBottom>
     </div>
   </div>
 </template>
 
+<style>
+@media screen and (min-width:480px) {
+
+  #overlay-topbanner{
+    display: none;
+  }
+
+  #overlay-bottombanner{
+    display: none;
+  }
+
+}
+
+</style>
+
 <script>
-import VueScriptInsert from './components/VueScriptInsert.vue'
+//import VueScriptInsert from './components/VueScriptInsert.vue'
+import OverlayAdvTop from './components/OverlayAdvTop.vue'
+import OverlayAdvBottom from './components/OverlayAdvBottom.vue'
 
 export default {
   name: 'App',
   components: {
-    VueScriptInsert
+    OverlayAdvTop,
+    OverlayAdvBottom
   },
   data() {
     return {
       adv_url_top:'<script>document.write("adv block");</scr'+'ipt>',
-      adv_id_top:'toppage-topbanner',
 
-      adv_layout:{
+      adv_id_top:'toppage-topbanner',
+      adv_id_overlay_top:'overlay-topbanner',
+
+      adv_id_bottom:'toppage-botbanner',
+      adv_id_overlay_bottom:'overlay-bottombanner',
+
+      /*adv_layout:{
           display: "block",
           margin: "0 auto", 
           background: "white",
           width: "1080px",
           height :"110px",
+        },*/
+      
+      adv_layout:{
+          display: "block",
+          margin: "0 auto", 
+          background: "white",
+          width: "360px",
+          height :"70px",
         },
     }
   }
---
+++ src/components/OverlayAdvTop.vue
@@ -0,0 +1,55 @@
+<template>
+<div :id="this.overlayid">
+    <div id="vuescriptinsert-overlay-block-top">
+        <VueScriptInsert :url="this.url" :id="this.id" :layout="this.layout"></VueScriptInsert>
+        <div class="vuescriptinsert-overlay-close-batsu" @click="close_overlay_top">×</div>
+    </div>
+</div>
+</template>
+
+
+<style>
+#vuescriptinsert-overlay-block-top {
+width: 100%;
+z-index: 9999;
+background: rgba(255, 255, 255, 0.92);
+position: fixed;
+left: 0;
+top: 0;
+height:auto;
+transition: all .3s ease;
+}
+
+#vuescriptinsert-overlay-block-top:hover {
+background: rgba(255, 255, 255, 0.48);
+}
+
+.vuescriptinsert-overlay-close-batsu{
+    font-size: 100%;
+    font-weight: bold;
+}
+</style>
+
+
+<script>
+import VueScriptInsert from './VueScriptInsert.vue'
+
+export default {
+    name:'OverlayAdvTop',
+    components: {
+        VueScriptInsert
+    },
+    props:['overlayid','url','id','layout'],
+    data(){
+        return{
+        }
+    },
+    methods: {
+        close_overlay_top(){
+            var elem = document.getElementById("vuescriptinsert-overlay-block-top")
+            elem.style.display='none'
+        }
+    }
+}
+</script>
+
